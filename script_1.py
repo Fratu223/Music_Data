85ccb795d91e180652755e216899454799741be6
Fratu <mihaifratu78@gmail.com>
2024-07-03 10:51:40 +0200

    Changes

--- script_1.py
+++ script_1.py
@@ -1,16 +1,38 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 # Title of the app
-st.title('CSV File Uploader')
+st.title('Spotify Playlist Data Analysis')
 
 # File uploader
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+uploaded_file = st.file_uploader("Choose a CSV playlist file", type="csv")
 
 if uploaded_file is not None:
     # Read the uploaded file
     df = pd.read_csv(uploaded_file)
     
     # Display the dataframe
-    st.write("Here's the data from the uploaded CSV file:")
+    st.write("Here's the data from the uploaded CSV playlist file:")
     st.write(df)
+
+    # Calculate the average tempo of the playlist
+    average_tempo = df.loc[:,'Tempo'].mean()
+
+    # Display the average tempo of the playlist
+    st.write("Average tempo of the playlist")
+    st.write(average_tempo)
+
+    # Calculate the average popularity of the playlist
+    average_popularity = df.loc[:,'Popularity'].mean()
+
+    # Display the average popularity of the playlist
+    st.write("Average popularity of the playlist")
+    st.write(average_popularity)
+
+    # Calculate the value couts for the artist name
+    artist_value_counts = df['Artist Name(s)'].value_counts()
+
+    # Plot the artist name by value counts
+    st.write('Value counts for artist name:')
+    st.bar_chart(artist_value_counts)
